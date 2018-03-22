5956d99a88b6aaaf3049f07f56c4acda63db3287
daltonmoore <daltonraymoore@gmail.com>
2018-03-22 14:15:11 -0400

    First Commit

---
+++ Galaga/Assets/Scripts/Enemy.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public GameObject bulletPrefab;
+    float timer = 0;
+    float bulletSpeed = 300;
+    float coneSize = 250;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        fire();
+    }
+
+    void fire()
+    {
+        if(timer+.75f+Random.value<Time.time)
+        {
+            timer = Time.time;
+            GameObject bulletInstance = Instantiate(bulletPrefab);
+            bulletInstance.transform.position = transform.position;
+            Rigidbody2D r = bulletInstance.GetComponent<Rigidbody2D>();
+
+            float width = Random.value * coneSize + (-Random.value * coneSize);
+            r.AddForce(Vector2.right * width + Vector2.down * bulletSpeed);
+
+            Destroy(bulletInstance, 2);
+        }
+    }
+    
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.tag == "bullet")
+        {
+            Destroy(gameObject);
+            Destroy(other.gameObject);
+        }
+    }
+}
---
+++ Galaga/Assets/Scripts/Player.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public GameObject bulletPrefab;
+    float speed = 1f / 12f;
+    float bulletSpeed = 225;
+
+	// Use this for initialization
+	void Start ()
+    {
+        
+	}
+
+	// Update is called once per frame
+	void Update ()
+    {
+        move();
+        fire();
+    }
+
+    void move()
+    {
+        float x = Input.GetAxis("Horizontal");
+
+        transform.Translate(x * speed, 0, 0);
+    }
+
+    void fire()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            GameObject bulletInstance = Instantiate(bulletPrefab);
+            bulletInstance.transform.position = transform.position;
+            Rigidbody2D r = bulletInstance.GetComponent<Rigidbody2D>();
+            r.AddForce(Vector2.up * bulletSpeed);
+            Destroy(bulletInstance, 2.5f);
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.tag == "enemyBullet")
+        {
+            Destroy(gameObject);
+        }
+    }
+}
